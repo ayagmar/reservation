7d10b1fef3fbce5c01af6617c675c06fb30edfb5
Abdeslam Yassine Agmar <Etu_AGMAR.AbdeslamYassine@emsi-edu.ma>
2021-07-09 09:55:58 +0100

    rename

--- src/main/java/com/adservio/reservation/ReservationSystemApplication.java
+++ src/main/java/com/adservio/reservation/ReservationSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookingSystemApplication {
+public class ReservationSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BookingSystemApplication.class, args);
+        SpringApplication.run(ReservationSystemApplication.class, args);
     }
 
 }
