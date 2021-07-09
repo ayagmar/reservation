75e94b7db9345e5a8c6315a4734adeb2af4d5f3d
Abdeslam Yassine Agmar <Etu_AGMAR.AbdeslamYassine@emsi-edu.ma>
2021-07-09 05:10:32 +0100

    adding files

---
+++ src/test/java/com/adservio/reservation/BookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adservio.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
